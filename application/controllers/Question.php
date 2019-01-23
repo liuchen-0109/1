d086239cc92c4979e95b2c0a260a0170e39eb756
liuchen <2354921286@qq.com>
2019-01-23 16:41:17 +0800

    修改配置

--- application/controllers/Question.php
+++ application/controllers/Question.php
@@ -5,10 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Question extends CI_Controller {
     public function index() {
-//       $data =  $this->getInput();
-        var_dump($_POST);
-        var_dump($_POST['title_one']);
-        var_dump($_POST['userInfo']->openId);
+        $data = [];
+        if($_POST['title_one'] && $_POST['one_A']&& $_POST['one_B']&& $_POST['one_C']){
+            $data['title_one'] = $_POST['title_one'];
+            $data['one_A'] = $_POST['one_A'];
+            $data['one_B'] = $_POST['one_B'];
+            $data['one_C'] = $_POST['one_C'];
+        }
+        var_dump($data);
     }
     public function haha() {
        echo '12312';
