c32d70eecb1593fd4cd3b41f8377b81271aa5c7b
liuchen <2354921286@qq.com>
2019-01-23 14:48:32 +0800

    修改配置

--- application/controllers/Question.php
+++ application/controllers/Question.php
@@ -5,8 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Question extends CI_Controller {
     public function index() {
-       $data =  $this->getInput();
-        var_dump($data);
+//       $data =  $this->getInput();
+//        var_dump($data);
+        var_dump($_POST);
     }
     public function haha() {
        echo '12312';
