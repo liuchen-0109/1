ff15b6bbf997e3f5984620e79fbd544c84bce1ae
liuchen <2354921286@qq.com>
2019-01-23 14:27:31 +0800

    修改配置

--- application/controllers/Question.php
+++ application/controllers/Question.php
@@ -5,6 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Question extends CI_Controller {
     public function index() {
+        $input = file_get_contents('php://input');
+        var_dump($input);exit;
         $userInfo = $_POST['userInfo'];
         $data = $_POST['data'];
         var_dump($userInfo);
