7a876792f8cc7dd1f0b203857dcf4af4202b8500
liuchen <2354921286@qq.com>
2019-01-24 09:34:30 +0800

    修改配置

--- application/controllers/Question.php
+++ application/controllers/Question.php
@@ -2,20 +2,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 use QCloud_WeApp_SDK\Model\Question as questionModel;
+
 class Question extends CI_Controller
 {
     public function index()
     {
-        if(!$_POST['openid']) return $this->outPut(1,'用户信息缺失');
+        if (!$_POST['openid']) return $this->outPut(1, '用户信息缺失');
         $data = $this->dealData();
-        if(!$data) return  $this->outPut(1,'数据不完整');
+        if (!$data) return $this->outPut(1, '数据不完整');
         $data['openid'] = $_POST['openid'];
-        $data['type'] = $_POST['type']?$_POST['type']:1;
-        $data['status'] = $_POST['status']?$_POST['status']:1;
+        $data['type'] = $_POST['type'] ? $_POST['type'] : 1;
+        $data['status'] = $_POST['status'] ? $_POST['status'] : 1;
         $data['create_time'] = $_SERVER['REQUEST_TIME'];
         $res = questionModel::storeQuestion($data);
-        if(!$res) return $this->outPut(1,'保存数据失败');
-        return $this->outPut(0,'成功');
+        if (!$res) return $this->outPut(1, '保存数据失败');
+        return $this->outPut(0, '成功');
     }
 
     public function dealData()
@@ -43,12 +44,50 @@ class Question extends CI_Controller
     }
 
 
-    function getQuestion(){
-        if(!isset($_POST['id'])) return  $this->outPut(1,'参数缺失');
+    function getQuestion()
+    {
+        if (!isset($_POST['id'])) return $this->outPut(1, '参数缺失');
         $res = questionModel::findQuestionById($_POST['id']);
-        var_dump($res);
-        if(!$res) return json_encode(['code'=>1,'msg'=>'没有查询到数据']);
-        return json_encode(['code'=>0,'data'=>$res]);
+        if (!$res) return json_encode(['code' => 1, 'msg' => '没有查询到数据']);
+        $data = $this->makeData($res);
+        return json_encode(['code' => 0, 'data' => $data]);
+    }
+
+    private function makeData($res)
+    {
+        $data = [];
+        if ($res['title_one']) {
+            $data['question']['one'] = [
+                'title' => $res['title_one'],
+                'A' => $res['one_A'],
+                'B' => $res['one_B'],
+                'C' => $res['one_C'],
+            ];
+        }
+        if ($res['title_two']) {
+            $data['question']['two'] = [
+                'title' => $res['title_two'],
+                'A' => $res['two_A'],
+                'B' => $res['two_B'],
+                'C' => $res['two_C'],
+            ];
+        }
+        if ($res['title_three']) {
+            $data['question']['three'] = [
+                'title' => $res['title_three'],
+                'A' => $res['three_A'],
+                'B' => $res['three_B'],
+                'C' => $res['three_C'],
+            ];
+        }
+        if ($res['title_one']) {
+            $data['question']['one'] = [
+                'title' => $res['title_one'],
+                'A' => $res['one_A'],
+                'B' => $res['one_B'],
+                'C' => $res['one_C'],
+            ];
+        }
     }
 
 }
