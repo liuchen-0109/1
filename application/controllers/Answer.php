b24ad5f8a92a02f458c936caae643c8a71d12f97
liuchen <2354921286@qq.com>
2019-01-24 16:14:34 +0800

    修改配置

--- application/controllers/Answer.php
+++ application/controllers/Answer.php
@@ -5,20 +5,21 @@ use QCloud_WeApp_SDK\Model\Answer as answerModel;
 
 class Answer extends CI_Controller
 {
-    function create_answer(){
+    function create_answer()
+    {
         if (!isset($_POST['pid'])) return $this->outPut(1, '题目信息缺失');
         if (!isset($_POST['openid'])) return $this->outPut(1, '用户信息缺失');
         $data = [];
         $data['openid'] = $_POST['openid'];
         $data['pid'] = $_POST['pid'];
-        $data['one'] = isset($_POST['one'])?$_POST['one']:0;
-        $data['two'] = isset($_POST['two'])?$_POST['two']:0;
-        $data['three'] = isset($_POST['three'])?$_POST['three']:0;
+        $data['one'] = $_POST['one'] != 0 ? $_POST['one'] : 0;
+        $data['two'] = $_POST['two'] != 0 ? $_POST['two'] : 0;
+        $data['three'] = $_POST['three'] != 0 ? $_POST['three'] : 0;
         $data['create_time'] = $_SERVER['REQUEST_TIME'];
         $data['status'] = 1;
 
-       $res =  answerModel::storeAnswer($data);
-       if(!$res) $this->outPut(1,'保存数据失败');
-       $this->outPut(0,"提交成功");
+        $res = answerModel::storeAnswer($data);
+        if (!$res) $this->outPut(1, '保存数据失败');
+        $this->outPut(0, "提交成功");
     }
 }
