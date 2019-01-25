a95b08085b140493db8fedf0034f1c9e0c4e09bd
liuchen <2354921286@qq.com>
2019-01-25 20:23:10 +0800

    1

--- vendor/qcloud/weapp-sdk/lib/Model/Question.php
+++ vendor/qcloud/weapp-sdk/lib/Model/Question.php
@@ -8,6 +8,7 @@ use \Exception;
 class Question
 {
     public static function storeQuestion ($data) {
+
           return  DB::insert('question', $data);
 
     }
@@ -16,6 +17,6 @@ class Question
         return DB::row('question', ['*'], compact('id'));
     }
     public static function findLast ($openid,$create_time) {
-        return DB::row('question', ['id'], compact('openid','create_time'),'and');
+        return DB::select('question', ['id'], compact('openid'));
     }
 }
