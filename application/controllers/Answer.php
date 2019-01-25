6e91dc4c66a1cf00a3ed7358b1f9fddcde7517bd
liuchen <2354921286@qq.com>
2019-01-25 11:16:20 +0800

    修改配置

--- application/controllers/Answer.php
+++ application/controllers/Answer.php
@@ -10,7 +10,7 @@ class Answer extends CI_Controller
         if (!isset($_POST['pid'])) return $this->outPut(1, '题目信息缺失');
         if (!isset($_POST['openid'])) return $this->outPut(1, '用户信息缺失');
         $search = answerModel::findAnswerByOpenidAndpid($_POST['openid'],$_POST['pid']);
-        if($search) return $this->outPut(1,'您已提交过此问题回答，请勿重新提交');
+//        if($search) return $this->outPut(1,'您已提交过此问题回答，请勿重新提交');
         $data = [];
         $data['openid'] = $_POST['openid'];
         $data['pid'] = $_POST['pid'];
