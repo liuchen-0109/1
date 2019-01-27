400e00fc179752ffabb827c0d9c3055bd46abb53
liuchen <2354921286@qq.com>
2019-01-27 19:00:12 +0800

    1

--- vendor/qcloud/weapp-sdk/lib/Model/Question.php
+++ vendor/qcloud/weapp-sdk/lib/Model/Question.php
@@ -27,7 +27,7 @@ class Question
 
     public static function findMyList($openid,$size,$offsize)
     {
-        $sql =  DB::raw('SELECT * FROM question WHERE `openid`=? AND `status`=1   ORDER BY create_time DESC ',[$openid] );
+        $sql =  DB::raw("SELECT * FROM question WHERE `openid`=? AND `status`=1 limit ?,?  ORDER BY create_time DESC ",[$openid,intval($size),intval($offsize)] );
         return $sql->fetchAll(2);
     }
 }
